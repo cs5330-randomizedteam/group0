72671ec868f901ddf86fe1228a9c32d764f4a17d
Wang Junming <905760127@qq.com>
2020-05-24 09:27:48 +0000

    init for proj1.task1

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -28,34 +28,47 @@ static bool load (const char *cmdline, void (**eip) (void), void **esp);
    before process_execute() returns.  Returns the new process's
    thread id, or TID_ERROR if the thread cannot be created. */
 tid_t
-process_execute (const char *file_name)
+process_execute (const int argc, const char **argv)
 {
-  char *fn_copy;
+  char **args_copy = palloc_get_page(0);
   tid_t tid;
+  ASSERT(argc > 0);
+  const char* file_name = argv[0];
 
   sema_init (&temporary, 0);
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
-  fn_copy = palloc_get_page (0);
-  if (fn_copy == NULL)
-    return TID_ERROR;
-  strlcpy (fn_copy, file_name, PGSIZE);
+  int i;
+  for (i = 0; i < argc; i++) {
+    args_copy[i] = palloc_get_page (0);
+    if (args_copy == NULL)
+      return TID_ERROR;
+    strlcpy (args_copy[i], argv[i], PGSIZE);
+  }
+  args_copy[i] = NULL;
 
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
-  if (tid == TID_ERROR)
-    palloc_free_page (fn_copy);
+  tid = thread_create (file_name, PRI_DEFAULT, start_process, args_copy);
+  if (tid == TID_ERROR) {
+    for (i = 0; i < argc; i++) {
+      palloc_free_page (args_copy[i]);
+    }
+  }
   return tid;
 }
 
 /* A thread function that loads a user process and starts it
    running. */
 static void
-start_process (void *file_name_)
+start_process (void *args_)
 {
-  char *file_name = file_name_;
+  char **args = args_;
+  char *file_name = args[0];
   struct intr_frame if_;
   bool success;
+
+  int i, argc = 0;
+  for (i = 0; args[i] != NULL; ++i) ++argc;
 
   /* Initialize interrupt frame and load executable. */
   memset (&if_, 0, sizeof if_);
@@ -63,7 +76,13 @@ start_process (void *file_name_)
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
   success = load (file_name, &if_.eip, &if_.esp);
-  if_.esp = (void*)((char*)if_.esp - (char*)20);
+
+  // if_.esp -= 12;
+  // *((char***)if_.esp) = args + 1;
+  // if_.esp -= 4;
+  // *((int*)if_.esp) = argc;
+  // if_.esp -= 4;
+  if_.esp -= 20;
 
   /* If load failed, quit. */
   palloc_free_page (file_name);
