a94fc39c0b4efc6052e546548cf034f55a0410f1
Wang Junming <905760127@qq.com>
2020-06-02 12:38:51 +0000

    fix memory leak bug

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -93,6 +93,8 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks)
 {
+  if (ticks <= 0) return;
+
   ASSERT (intr_get_level () == INTR_ON);
   struct sleeping_thread *sleep_thread;
   sleep_thread = malloc(sizeof(struct sleeping_thread));
@@ -103,6 +105,7 @@ timer_sleep (int64_t ticks)
   old_level = intr_disable ();
   list_push_back(&sleep_list, &(sleep_thread->elem));
   thread_block();
+  free(sleep_thread);
   intr_set_level (old_level);
 }
 
